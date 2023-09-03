95fb26c108867fa83eb17a74d5b4ec201bd38bb3
Elaidzha1940 <eli080293@gmail.com>
2023-09-03 16:16:38 +0300

    AnimatedGifs

---
+++ AnimatedGifs/AnimatedGifsApp.swift
@@ -0,0 +1,21 @@
+//  /*
+//
+//  Project: AnimatedGifs
+//  File: AnimatedGifsApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 03.09.2023
+//
+//  Status: in progress | Decorated
+//
+//  */
+
+import SwiftUI
+
+@main
+struct AnimatedGifsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimatedGifs/ContentView.swift
@@ -0,0 +1,28 @@
+//  /*
+//
+//  Project: AnimatedGifs
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 03.09.2023
+//
+//  Status: in progress | Decorated
+//
+//  */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+           
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
