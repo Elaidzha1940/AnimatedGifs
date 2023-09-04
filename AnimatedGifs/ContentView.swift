7d68443a45109db33686bfa2948aff1f14e425ae
Elaidzha1940 <eli080293@gmail.com>
2023-09-04 14:17:02 +0300

    AnimatedGifs + added Button

--- AnimatedGifs/ContentView.swift
+++ AnimatedGifs/ContentView.swift
@@ -10,15 +10,27 @@
 //  */
 
 import SwiftUI
-import UIKit
+import WebKit
 
 struct ContentView: View {
+    @State private var showWebView = false
+
     var body: some View {
         
         VStack {
-           
-            
+            if showWebView {
+                AnimatedGIFView(gifName: "200w")
+                    .frame(width: 200, height: 200)
+                    .cornerRadius(20)
+            } else {
+                Button("Open Fire") {
+                    showWebView.toggle()
+                }
+                .foregroundColor(.white)
+                .font(.system(size: 20, weight: .bold, design: .serif))
+            }
         }
+        .preferredColorScheme(.dark)
     }
 }
 
@@ -28,4 +40,19 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-
+struct AnimatedGIFView: UIViewRepresentable {
+    let gifName: String
+    
+    func makeUIView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        return webView
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let gifPath = Bundle.main.path(forResource: gifName, ofType: "gif") {
+            let gifURL = URL(fileURLWithPath: gifPath)
+            let request = URLRequest(url: gifURL)
+            uiView.load(request)
+        }
+    }
+}
